3708b96196585eece739e8b1697bd11517c3cb95
DESKTOP-4M08K52\USER <asj4925@gmail.com>
2022-12-19 13:28:43 +0900

    node-sass 설치 & 상단 ui

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import './App.scss';
 
 function App() {
+  const [income, setIncome] = React.useState(300);
+  const [expense, setExpense] = React.useState(20); 
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+
   return (
     <div className="App">
-      Money Note
+      <div id="mNote">
+        <div className="top-container">
+          <p className="month">{year}년 {month}월</p>
+          <p className="my-money">{income - expense}원</p>
+          <div className="in-out">
+            <dl className="income">
+              <dt>수입</dt>
+              <dd>{income}원</dd>
+            </dl>
+            <dl className="expense">
+              <dt>지출</dt>
+              <dd>{expense}원</dd>
+            </dl>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
